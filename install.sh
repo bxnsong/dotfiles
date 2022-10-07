87ec4483fdcb8af23dce9881cfa62d3f8ed68a5b
Ben Soh <ben.soh@discordapp.com>
2022-10-07 16:48:26 -0700

    install fd and rg

--- install.sh
+++ install.sh
@@ -6,3 +6,15 @@ sudo apt-get install -y neovim
 # LSD
 LSD_DEB_PATH=/home/discord/installation/lsd-0.23.1.deb
 sudo dpkg -i $LSD_DEB_PATH
+
+# ripgrep
+if ! command -v rg &> /dev/null
+then
+    cargo install ripgrep
+fi
+
+# fd
+if ! command -v fd &> /dev/null
+then
+    cargo install fd-find
+fi

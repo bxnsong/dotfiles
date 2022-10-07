d61f2f3a9a5b751cd095c92c3bbdeb8227909af5
Ben Soh <ben.soh@discordapp.com>
2022-10-07 16:37:54 -0700

    install lsd

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+# Neovim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
-sudo apt-get install -y silversearcher-ag neovim
+sudo apt-get install -y neovim
+
+# LSD
+LSD_DEB_PATH=/home/discord/installation/lsd-0.23.1.deb
+sudo dpkg -i $LSD_DEB_PATH

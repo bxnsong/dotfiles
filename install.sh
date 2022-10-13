adc554bfd2ffb305fe0e7ec916d459ae1fb8ff83
Ben Soh <ben.soh@discordapp.com>
2022-10-13 07:15:33 +0000

    add log statements

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ dotfiles_home=~/dotfiles
 # Neovim
 if ! [ -x "$(command -v nvim)" ]
 then
+    echo "neovim not found, installing"
     sudo add-apt-repository ppa:neovim-ppa/unstable
     sudo apt-get update
     sudo apt-get install -y neovim
@@ -12,6 +13,7 @@ fi
 # LSD
 if ! [ -x "$(command -v lsd)" ]
 then
+    echo "lsd not found, installing"
     lsd_deb_path=/home/discord/installation/lsd-0.23.1.deb
     sudo dpkg -i $lsd_deb_path
 fi
@@ -19,16 +21,19 @@ fi
 # ripgrep
 if ! [ -x "$(command -v rg)" ]
 then
+    echo "rg not found, installing"
     cargo install ripgrep
 fi
 
 # fd
 if ! [ -x "$(command -v fd)" ]
 then
+    echo "fd not found, installing"
     cargo install fd-find
 fi
 
 # copy nvim config
+echo "symlinking .config"
 packer_home=~/.local/share/nvim/site/pack/packer/start/packer.nvim
 if ! [ -d "$packer_home" ]; then
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim $packer_home
@@ -37,6 +42,7 @@ cp -rsf "$dotfiles_home"/.config/. ~/.config
 
 # fasd
 if ! [ -x "$(command -v fasd)" ]; then
+    echo "fasd not found, installing"
     sudo add-apt-repository ppa:aacebedo/fasd
     sudo apt-get update
     sudo apt-get install -y fasd
@@ -44,5 +50,6 @@ fi
 
 # prettier
 if ! [ -x "$(command -v prettier)" ]; then
+    echo "prettier not found, installing"
     sudo npm i prettier -g
 fi

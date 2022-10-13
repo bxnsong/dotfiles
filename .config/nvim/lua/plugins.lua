87b9b2fc8c1ce234b5dcede462249be642ace95f
Ben Soh <ben.soh@discordapp.com>
2022-10-13 08:35:06 +0000

    format on save

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,6 +8,25 @@ return require('packer').startup(function()
     use { 'neovim/nvim-lspconfig' }
 
     use { 'jose-elias-alvarez/null-ls.nvim', config = function() require'null-ls'.setup({
+        -- you can reuse a shared lspconfig on_attach callback here
+        on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                        vim.lsp.buf.format({
+                            bufnr = bufnr,
+                            filter = function(client)
+                                return client.name == 'null-ls'
+                            end
+                        })
+                    end,
+                })
+            end
+        end,
         sources = {
             require'null-ls'.builtins.formatting.prettierd.with({
                 env = {
@@ -15,7 +34,7 @@ return require('packer').startup(function()
                 }
             }),
         },
-    }) end }
+    }) end, requires = { {'nvim-lua/plenary.nvim'} } }
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
@@ -60,37 +79,37 @@ return require('packer').startup(function()
     use { 'steelsojka/pears.nvim', config = function() require'pears'.setup() end }
 
     use {'datwaft/bubbly.nvim', config = function()
-    -- Here you can add the configuration for the plugin
-    vim.g.bubbly_palette = {
-      background = "#34343c",
-      foreground = "#c5cdd9",
-      black = "#3e4249",
-      red = "#ec7279",
-      green = "#a0c980",
-      yellow = "#deb974",
-      blue = "#6cb6eb",
-      purple = "#d38aea",
-      cyan = "#5dbbc1",
-      white = "#c5cdd9",
-      lightgrey = "#57595e",
-      darkgrey = "#404247",
-    }
-    vim.g.bubbly_statusline = {
-      'mode',
+        -- Here you can add the configuration for the plugin
+        vim.g.bubbly_palette = {
+            background = "#34343c",
+            foreground = "#c5cdd9",
+            black = "#3e4249",
+            red = "#ec7279",
+            green = "#a0c980",
+            yellow = "#deb974",
+            blue = "#6cb6eb",
+            purple = "#d38aea",
+            cyan = "#5dbbc1",
+            white = "#c5cdd9",
+            lightgrey = "#57595e",
+            darkgrey = "#404247",
+        }
+        vim.g.bubbly_statusline = {
+            'mode',
 
-      'truncate',
+            'truncate',
 
-      'path',
-      'branch',
-      'signify',
-      'gitsigns',
-      'coc',
+            'path',
+            'branch',
+            'signify',
+            'gitsigns',
+            'coc',
 
-      'divisor',
+            'divisor',
 
-      'filetype',
-      'progress',
-    }
-  end}
+            'filetype',
+            'progress',
+        }
+    end}
 
 end)

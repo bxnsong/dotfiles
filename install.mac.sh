73a0d998afc39f29da84b1086a94883413df19e9
Ben Soh <ben.soh@discordapp.com>
2022-10-16 05:56:11 -0700

    add some mac defaults

--- install.mac.sh
+++ install.mac.sh
@@ -84,3 +84,27 @@ if ! [ -x "$(command -v fpp)" ]; then
     echo "fpp not found, installing"
     brew install fpp
 fi
+
+# save screenshots as .png
+defaults write com.apple.screencapture type -string "png"
+
+# set screenshot location to ~/Pictures/screenshots
+mkdir -p ~/Pictures/screenshots
+screenshot_location=~/Pictures/screenshots/
+defaults write com.apple.screencapture location -string "${screenshot_location}"
+
+# show file extensions in Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# enable keyboard for UI elements (e.g. tab works for modal dialogs, menu windows)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# make keyboard repeat rate fast
+defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write -g InitialKeyRepeat -int 15
+
+# auto-hide dock
+defaults write com.apple.dock autohide -bool true
+
+# unshow recents in dock
+defaults write com.apple.dock show-recents -bool FALSE

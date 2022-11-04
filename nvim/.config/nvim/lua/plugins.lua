15c8bc2f49370ca3ebe7ef5badba6dbe32b28b17
Ben Soh <ben.soh@discordapp.com>
2022-11-04 05:17:18 -0700

    add ktlint

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -50,6 +50,8 @@ return require('packer').startup(function()
             require'null-ls'.builtins.diagnostics.eslint_d,
             require'null-ls'.builtins.formatting.black,
             require'null-ls'.builtins.diagnostics.flake8,
+            require'null-ls'.builtins.formatting.ktlint,
+            require'null-ls'.builtins.diagnostics.ktlint,
         },
     }) end, requires = { {'nvim-lua/plenary.nvim'} }, after = 'mason.nvim' }
 

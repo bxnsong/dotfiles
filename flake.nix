fe9a201ea50b32a613e688f8b781d01b9f787c3d
Ben Soh <ben.soh@discordapp.com>
2023-03-27 01:24:43 -0700

    [nix] clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,5 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-      home = (import ./home.nix { inherit pkgs; });
-    in { homeConfigurations = import ./configs inputs; };
+  outputs = inputs: { homeConfigurations = import ./configs inputs; };
 }

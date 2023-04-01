fad1f0bafe21558a7baa7098e8ca291adfa37e48
Ben Soh <ben.soh@discordapp.com>
2023-03-31 20:04:40 -0700

    python: add lxml

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 let
   python3 = pkgs.python3.withPackages
-    (p: with p; [ flake8 requests python-lsp-server pynvim ipython ]);
+    (p: with p; [ flake8 lxml requests python-lsp-server pynvim ipython ]);
 in { home = { packages = with pkgs; [ python3 ]; }; }

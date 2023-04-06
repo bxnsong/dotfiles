c3877e2eedabfa2d8c000c81788477592d0e6f58
Ben Soh <ben.soh@discordapp.com>
2023-04-06 03:38:38 -0700

    nix: fix nixpkgs.overlays

--- home-manager/configs/default.nix
+++ home-manager/configs/default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs, home-manager, ... }@inputs:
+{ nixpkgs, neovim-nightly-overlay, home-manager, ... }@inputs:
 let
   localLib = import ../../lib { inherit (nixpkgs) lib; };
   inherit (builtins) mapAttrs;
@@ -7,12 +7,12 @@ let
 in mapAttrs (_: host:
   (let
     inherit (host) system;
-    pkgs = import nixpkgs { inherit (host) system; };
+    pkgs = import nixpkgs {
+      inherit (host) system;
+      overlays = [ neovim-nightly-overlay.overlay ];
+    };
   in home-manager.lib.homeManagerConfiguration {
     inherit pkgs;
     extraSpecialArgs = inputs // { inherit (host.config) ben; };
     modules = [ ../modules/default.nix ];
-    configuration = { pkgs, ... }: {
-      nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ];
-    };
   })) hosts

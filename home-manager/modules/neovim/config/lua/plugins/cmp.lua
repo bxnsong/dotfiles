20414534ef97c05760961420007d3782c48dff59
Ben Soh <ben.soh@discordapp.com>
2023-05-14 03:38:25 -0700

    nvim: fix luasnippet weird tab behaviour

--- home-manager/modules/neovim/config/lua/plugins/cmp.lua
+++ home-manager/modules/neovim/config/lua/plugins/cmp.lua
@@ -1,3 +1,16 @@
+vim.api.nvim_create_autocmd("ModeChanged", {
+  pattern = "*",
+  callback = function()
+    if
+      ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+      and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+      and not require("luasnip").session.jump_active
+    then
+      require("luasnip").unlink_current()
+    end
+  end,
+})
+
 return {
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip

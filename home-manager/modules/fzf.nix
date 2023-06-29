67db3c22acdd76d1e2841437ecdd179284c02940
Ben Soh <ben.soh@discordapp.com>
2023-06-28 22:58:22 -0700

    fzf: use catppuccin

--- home-manager/modules/fzf.nix
+++ home-manager/modules/fzf.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }: {
   programs = {
-    fzf = { enable = true; };
-    zsh.sessionVariables = {
-      FZF_DEFAULT_COMMAND =
-        "fd --type f --color=always' --exclude .git --ignore-file ~/.gitignore";
+    fzf = {
+      enable = true;
+      defaultCommand =
+        "fd --type f --color=always --exclude .git --ignore-file ~/.gitignore";
+      colors = {
+        bg = "#1e1e2e";
+        "bg+" = "#313244";
+        spinner = "#f5e0dc";
+        hl = "#f38ba8";
+        fg = "#cdd6f4";
+        header = "#f38ba8";
+        info = "#cba6f7";
+        pointer = "#f5e0dc";
+        marker = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        prompt = "#cba6f7";
+        "hl+" = "#f38ba8";
+      };
     };
   };
 }

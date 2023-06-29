b72c9cc5756fbecf21bf24216eb626179c7a4980
Ben Soh <ben.soh@discordapp.com>
2023-06-28 23:37:03 -0700

    nvim: fix colorizer

--- home-manager/modules/neovim/config/lua/plugins/file.lua
+++ home-manager/modules/neovim/config/lua/plugins/file.lua
@@ -1,6 +1,11 @@
 return {
   { "gpanders/editorconfig.nvim", event = { "BufReadPre", "BufNewFile" } },
-  { "norcalli/nvim-colorizer.lua", config = true },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
   {
     "luukvbaal/nnn.nvim",
     config = {

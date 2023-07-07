579a1626cc83313b90b9d374bc645f13d90e2c54
Ben Soh <ben.soh@discordapp.com>
2023-07-07 01:08:25 -0700

    nvim: fix colourscheme loading

--- home-manager/modules/neovim/config/lua/config/lazy.lua
+++ home-manager/modules/neovim/config/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "akinsho/bufferline.nvim", enabled = false },
+    { "catppuccin", enabled = false },
   },
   defaults = {
     lazy = false,
--- home-manager/modules/neovim/config/lua/plugins/catppuccinight.lua
+++ home-manager/modules/neovim/config/lua/plugins/catppuccinight.lua
@@ -1,6 +1,7 @@
 return {
   {
     "m4xshen/catppuccinight.nvim",
+    lazy = true,
     name = "catppuccinight",
     opts = {
       flavour = "mocha",
@@ -9,8 +10,36 @@ return {
           VertSplit = { fg = colours.surface0 },
         }
       end,
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
     },
-    init = function()
+    config = function()
       vim.cmd.colorscheme("catppuccin")
     end,
   },

4a6c904935982b15465e06c34c7e69d9145f5da6
Ben Soh <ben.soh@discordapp.com>
2024-01-12 17:02:42 +0900

    nvim: add TreeSJ

--- home-manager/modules/neovim/config/lua/plugins/plugins.lua
+++ home-manager/modules/neovim/config/lua/plugins/plugins.lua
@@ -153,6 +153,14 @@ return {
   },
 
   {
+    "Wansmer/treesj",
+    keys = {
+      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    },
+    opts = { use_default_keymaps = false, max_join_length = 150 },
+  },
+
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {

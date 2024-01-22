6564664afbcec3ae24bdf2eaddc2fb3b67e6434a
Ben Soh <ben.soh@discordapp.com>
2024-01-22 03:51:43 -0800

    nvim: harpoon

--- home-manager/modules/neovim/config/lua/plugins/base.lua
+++ home-manager/modules/neovim/config/lua/plugins/base.lua
@@ -257,4 +257,89 @@ return {
       vim.keymap.set("", "<f1>", toggle_profile)
     end,
   },
+
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup({})
+
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+        local file_paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(file_paths, item.value)
+        end
+
+        require("telescope.pickers")
+          .new({}, {
+            prompt_title = "Harpoon",
+            finder = require("telescope.finders").new_table({
+              results = file_paths,
+            }),
+            previewer = conf.file_previewer({}),
+            sorter = conf.generic_sorter({}),
+          })
+          :find()
+      end
+      vim.keymap.set("n", "<leader>hl", function()
+        toggle_telescope(require("harpoon"):list())
+      end, { desc = "Open Harpoon window" })
+    end,
+    keys = {
+      {
+        "<leader>hL",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Open Harpoon window",
+      },
+      {
+        "<leader>ha",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "Append file to Harpoon",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Jump to Harpoon file 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Jump to Harpoon file 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Jump to Harpoon file 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Jump to Harpoon file 4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "Jump to Harpoon file 5",
+      },
+    },
+  },
 }

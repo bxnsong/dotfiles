06f6e93c7799bb79e2614a035416a1f2b026d064
Ben Soh <ben.soh@discordapp.com>
2024-05-23 01:57:24 -0700

    aerospace: borders and sketchybar

---
+++ home-manager/modules/darwin/sketchybar/items/aerospace.sh
@@ -0,0 +1,14 @@
+sketchybar --add event aerospace_workspace_change
+
+for sid in $(/opt/homebrew/bin/aerospace list-workspaces --all); do
+    sketchybar --add item space.$sid left \
+        --subscribe space.$sid aerospace_workspace_change \
+        --set space.$sid \
+        background.color=0x44ffffff \
+        background.corner_radius=5 \
+        background.height=20 \
+        background.drawing=off \
+        label="$sid" \
+        click_script="/opt/homebrew/bin/aerospace workspace $sid" \
+        script="$PLUGIN_DIR/aerospace.sh $sid"
+done

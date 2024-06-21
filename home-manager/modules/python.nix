bd6b61514849c42d9e0d719517c5cbaf1b9fa11f
Ben Soh <ben.soh@discordapp.com>
2024-06-21 14:09:45 -0700

    python: install pipx and pyenv

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -1,3 +1,3 @@
 { pkgs, ben, ... }:
 let python3 = pkgs.python311.withPackages (p: with p; [ pynvim ipython pipx ]);
-in { home = { packages = with pkgs; if !ben.work then [ python3 ] else [ ]; }; }
+in { home = { packages = with pkgs; if !ben.work then [ python3 ] else [ pyenv pipx ]; }; }

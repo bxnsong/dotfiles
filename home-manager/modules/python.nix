91852f87805f143faae759289b3dd5465a47aded
Ben Soh <ben.soh@discordapp.com>
2024-09-28 01:53:28 -0700

    pyenv: use home manager pyenv package

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -1,3 +1,6 @@
 { pkgs, ben, ... }:
 let python3 = pkgs.python311.withPackages (p: with p; [ pynvim ipython pipx ]);
-in { home = { packages = with pkgs; if !ben.work then [ python3 pyenv ] else [ pyenv pipx ]; }; }
+in {
+  home = { packages = with pkgs; if !ben.work then [ ] else [ pipx ]; };
+  programs = { pyenv = { enable = true; }; };
+}

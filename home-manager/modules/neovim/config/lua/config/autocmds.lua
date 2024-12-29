846525458fdd65ce0fa535ceff7b3c5bb9007d72
Ben Soh <ben.soh@discordapp.com>
2024-12-28 16:20:01 -0800

    nvim: better diff colours

--- home-manager/modules/neovim/config/lua/config/autocmds.lua
+++ home-manager/modules/neovim/config/lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+function DiffColours()
+  vim.api.nvim_set_hl(0, "DiffChange", { bg = "#25274c", reverse = false })
+  vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#223342", reverse = false })
+  vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#362433", reverse = false })
+  vim.api.nvim_set_hl(0, "DiffviewAddAsDelete", { bg = "#362433", reverse = false })
+  vim.api.nvim_set_hl(0, "DiffText", { bg = "#38396f", reverse = false })
+  vim.api.nvim_set_hl(0, "DiffviewDiffDeleteDim", { fg = "#161d2d", reverse = false })
+end
+
+DiffColours()
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = { "calvera" },
+  callback = DiffColours,
+})

d8061eb28888e09327f3ff16e7fa5d08496f244c
Ben Soh <ben.soh@discordapp.com>
2025-01-25 08:36:36 -0800

    Revert "sketchybar: add PT time temporarily"
    This reverts commit b5074a309699af8acef265c35dabb52058a88aa3.

--- home-manager/modules/darwin/sketchybar/plugins/clock.sh
+++ home-manager/modules/darwin/sketchybar/plugins/clock.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-sketchybar --set $NAME label="$(date '+%a %b %-d %-H:%M') (PT $(TZ='America/Los_Angeles' date '+%a %b %-d %-H:%M'))"
+sketchybar --set $NAME label="$(date '+%a %b %-d %-H:%M')"

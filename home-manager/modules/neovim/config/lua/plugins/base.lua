e7f2a77ce694b679d4ae0e9d70ebc36438fd8e1d
Ben Soh <ben.soh@discordapp.com>
2025-03-04 12:33:54 -0800

    nvim: output panel

--- home-manager/modules/neovim/config/lua/plugins/base.lua
+++ home-manager/modules/neovim/config/lua/plugins/base.lua
@@ -5,6 +5,26 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   {
+    "mhanberg/output-panel.nvim",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("output_panel").setup({
+        max_buffer_size = 5000, -- default
+      })
+    end,
+    cmd = { "OutputPanel" },
+    keys = {
+      {
+        "<leader>o",
+        vim.cmd.OutputPanel,
+        mode = "n",
+        desc = "Toggle the output panel",
+      },
+    },
+  },
+
+  {
     "rafamadriz/friendly-snippets",
     enabled = false,
   },

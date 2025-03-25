7d5c1fc441f84a359e39691cb6deff979662c108
Ben Soh <ben.soh@discordapp.com>
2025-03-25 16:51:30 -0700

    nvim: remove output-panel

--- home-manager/modules/neovim/config/lua/plugins/base.lua
+++ home-manager/modules/neovim/config/lua/plugins/base.lua
@@ -6,26 +6,6 @@ return {
   { "echasnovski/mini.animate", cond = not vim.g.vscode },
 
   {
-    "mhanberg/output-panel.nvim",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("output_panel").setup({
-        max_buffer_size = 5000, -- default
-      })
-    end,
-    cmd = { "OutputPanel" },
-    keys = {
-      {
-        "<leader>o",
-        vim.cmd.OutputPanel,
-        mode = "n",
-        desc = "Toggle the output panel",
-      },
-    },
-  },
-
-  {
     "rafamadriz/friendly-snippets",
     enabled = false,
   },

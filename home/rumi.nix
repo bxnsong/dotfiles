0bddefd16e36e19b331ccf4ea2cc4fe2f1caa67b
Ben Soh <ben.soh@usepylon.com>
2025-09-02 13:29:08 -0700

    feat(*): add work packages

--- home/rumi.nix
+++ home/rumi.nix
@@ -1,4 +1,25 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ slack ];
+  home.packages = with pkgs; [
+    awscli
+    caddy
+    colima
+    docker
+    fzf
+    graphviz
+    jq
+    mycli
+    mysql84
+    pulumi
+    pyenv
+    redis
+    slack
+    wget
+  ];
+
   programs.git = { userEmail = "ben.soh@usepylon.com"; };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 }
